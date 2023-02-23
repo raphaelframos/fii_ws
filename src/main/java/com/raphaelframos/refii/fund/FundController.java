957261dd1071188dad0e571e79c775dc2b301816
Raphael Ferreira Ramos <raphaelframos@gmail.com>
2023-02-23 15:12:08 -0300

    Corrigindo importacoes

--- src/main/java/com/raphaelframos/refii/fund/FundController.java
+++ src/main/java/com/raphaelframos/refii/fund/FundController.java
@@ -1,6 +1,5 @@
 package com.raphaelframos.refii.fund;
 
-import com.raphaelframos.refii.common.model.ChatResponse;
 import com.raphaelframos.refii.common.service.FundService;
 import com.raphaelframos.refii.fund.model.FundDetail;
 import com.raphaelframos.refii.fund.model.FundResponse;
@@ -9,8 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Collections;
 import java.util.List;
 
